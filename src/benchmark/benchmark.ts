ebaa6ca30337298b94dd9c8db2e5832d48c4c667
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2022-08-03 14:26:02 +0200

    updated benchmark file

--- src/benchmark/benchmark.ts
+++ src/benchmark/benchmark.ts
@@ -2,17 +2,8 @@
  * - file to run benchmarks
  */
 
-import { benchmark } from "../deps.ts";
 import { generateUUID } from "../utils/index.ts";
 
-benchmark.bench({
-    name: "run UUID generate function 1000times",
-    runs: 1000,
-    func: (b) => {
-        b.start();
-        generateUUID();
-        b.stop();
-    }
+Deno.bench("run UUID generate function", () => {
+  generateUUID();
 });
-
-benchmark.runBenchmarks();
