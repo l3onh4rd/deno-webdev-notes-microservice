f5d5fa63a265eadac150837120d9d89dc46ba6c6
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2023-04-15 22:01:25 +0200

    added password and username validation to benchmarks

--- src/benchmark/benchmark.ts
+++ src/benchmark/benchmark.ts
@@ -2,8 +2,16 @@
  * - file to run benchmarks
  */
 
-import { generateUUID } from "../utils/index.ts";
+import { generateUUID, validatePassword, validateUsername } from "../utils/index.ts";
 
-Deno.bench("run UUID generate function", () => {
+Deno.bench("run UUID generation", () => {
   generateUUID();
+});
+
+Deno.bench("run password validation", () => {
+  validatePassword('tHiSIsAR4nd0MP§//W4rd');
+});
+
+Deno.bench("run username validation", () => {
+  validateUsername('r4andU53rname(');
 });
