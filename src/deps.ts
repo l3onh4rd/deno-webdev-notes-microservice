e27f2a44a7a7e31414ce2033ea96c1c0a139e991
l3onh4rd <46918465+l3onh4rd@users.noreply.github.com>
2024-05-06 09:36:55 +0200

    updated deno 1.43.1 and std to 0.224.0

--- src/deps.ts
+++ src/deps.ts
@@ -22,5 +22,5 @@ export {
 export type { Header, Payload } from "https://deno.land/x/djwt@v2.4/mod.ts";
 
 // deno std dependencies
-export { v4 } from "https://deno.land/std@0.222.1/uuid/mod.ts";
-export * as t from "https://deno.land/std@0.222.1/testing/asserts.ts";
+export { v4 } from "https://deno.land/std@0.224.0/uuid/mod.ts";
+export * as t from "https://deno.land/std@0.224.0/testing/asserts.ts";
